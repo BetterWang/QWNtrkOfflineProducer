8c7085f932a52087878e56c939c5f7ec17487ca6
Quan Wang <BetterWang@gmail.com>
2016-10-10 16:38:38 +0200

    [+] comments

--- python/QWMult_cfi.py
+++ python/QWMult_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+# Multiplicity, 0.3 < pT < 3.0, |eta| < 2.4
 QWMult = cms.EDProducer("QWMultProducer",
 		vertexSrc = cms.untracked.InputTag("offlinePrimaryVertices"),
 		trackSrc  = cms.untracked.InputTag("generalTracks"),
--- python/QWNoff_cfi.py
+++ python/QWNoff_cfi.py
@@ -1,5 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
+# N_{trk}^{offline}, pT > 0.4, |eta| < 2.4
 QWNoff = cms.EDProducer("QWNtrkOfflineProducer",
 		vertexSrc = cms.untracked.InputTag("offlinePrimaryVertices"),
 		trackSrc  = cms.untracked.InputTag("generalTracks")
