27d3aa917bf910e3231abe04f2f319ba2d836cc4
Quan Wang <BetterWang@gmail.com>
2016-11-25 22:34:56 +0100

    [+] add more

---
+++ src/QWEventInfoProducer.cc
@@ -0,0 +1,69 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "TFile.h"
+#include "TH2.h"
+#include "TMath.h"
+
+using namespace std;
+class QWEventInfoProducer : public edm::EDProducer {
+public:
+	explicit QWEventInfoProducer(const edm::ParameterSet&);
+	~QWEventInfoProducer();
+
+private:
+	virtual void produce(edm::Event&, const edm::EventSetup&) override;
+	///
+};
+
+QWEventInfoProducer::QWEventInfoProducer(const edm::ParameterSet& pset)
+{
+	produces< double >("RunId");
+	produces< double >("EventId");
+	produces< double >("Lumi");
+	produces< double >("BX");
+	produces< double >("Orbit");
+}
+
+QWEventInfoProducer::~QWEventInfoProducer()
+{
+	return;
+}
+
+void QWEventInfoProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	using namespace edm;
+	using namespace reco;
+
+	double runid = iEvent.getRun();
+	double eventid = iEvent.id();
+	double bx = iEvent.bunchCrossing();
+	double lumi = iEvent.luminosityBlock();
+	double orbit = iEvent.orbitNumber();
+
+	std::auto_ptr<double> prunid(new double(runid));
+	std::auto_ptr<double> peventid(new double(eventid));
+	std::auto_ptr<double> pbx(new double(bx));
+	std::auto_ptr<double> plumi(new double(lumi));
+	std::auto_ptr<double> porbit(new double(orbit));
+
+	iEvent.put(prunid, std::string("RunId"));
+	iEvent.put(peventid, std::string("EventId"));
+	iEvent.put(pbx, std::string("BX"));
+	iEvent.put(plumi, std::string("Lumi"));
+	iEvent.put(porbit, std::string("Orbit"));
+}
+
+
+DEFINE_FWK_MODULE(QWEventInfoProducer);
---
+++ src/QWVectorSelector.cc
@@ -0,0 +1,63 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/TrackReco/interface/Track.h"
+#include "DataFormats/TrackReco/interface/TrackFwd.h"
+#include "DataFormats/VertexReco/interface/Vertex.h"
+#include "DataFormats/VertexReco/interface/VertexFwd.h"
+
+#include "TFile.h"
+#include "TH2.h"
+#include "TMath.h"
+
+using namespace std;
+class QWVectorSelector : public edm::EDProducer {
+public:
+	explicit QWVectorSelector(const edm::ParameterSet&);
+	~QWVectorSelector();
+
+private:
+	virtual void produce(edm::Event&, const edm::EventSetup&) override;
+	///
+
+	edm::InputTag	vectSrc_;
+	int	idx_;
+};
+
+QWVectorSelector::QWVectorSelector(const edm::ParameterSet& pset) :
+	vectSrc_(pset.getUntrackedParameter<edm::InputTag>("vectSrc")),
+	idx_(pset.getUntrackedParameter<int>("idx", 0),
+{
+	consumes< std::vector<double> >(vectSrc_);
+	produces<double>();
+}
+
+QWVectorSelector::~QWVectorSelector()
+{
+	return;
+}
+
+void QWVectorSelector::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	using namespace edm;
+
+	Handle<std::vector<double> > t;
+	iEvent.getByLabel(vectSrc_, t);
+
+	double ret;
+	if ( t->size() <= idx ) ret = -9999999;
+	else ret = (*t)[idx];
+
+	std::auto_ptr<double> pret(new double(ret));
+
+	iEvent.put(pret);
+}
+
+
+DEFINE_FWK_MODULE(QWVectorSelector);
