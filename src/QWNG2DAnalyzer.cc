cca604bbdf35d376d1cac18cc7debc1444e44dde
Quan Wang <BetterWang@gmail.com>
2016-11-25 16:27:57 +0100

    [+] update

--- src/QWNG2DAnalyzer.cc
+++ src/QWNG2DAnalyzer.cc
@@ -11,16 +11,17 @@
 #include <iostream>
 #include <tuple>
 
-class QW2DAnalyzer : public edm::EDAnalyzer {
+class QWNG2DAnalyzer : public edm::EDAnalyzer {
 public:
-	explicit QW2DAnalyzer(const edm::ParameterSet&);
-	~QW2DAnalyzer() {};
+	explicit QWNG2DAnalyzer(const edm::ParameterSet&);
+	~QWNG2DAnalyzer() {};
 private:
 	virtual void beginJob() {};
 	virtual void analyze(const edm::Event&, const edm::EventSetup&);
 	virtual void endJob() {};
 
 	edm::InputTag   srcPhi_;
+	edm::InputTag   srcPt_;
 	edm::InputTag   srcEta_;
 	edm::InputTag   srcW_;
 	TH1D * h;
@@ -29,19 +30,27 @@ private:
 	bool	bWeight;
 
 	std::vector<double>	mphi_;
+	std::vector<double>	mpt_;
 	std::vector<double>	meta_;
 	std::vector<double>	mw_;
+
+	double trigPtMin_;
+	double trigPtMax_;
+	double assoPtMin_;
+	double assoPtMax_;
 
 	const double DphiMax = 2*TMath::Pi() - 2;
 	const double DphiMin = -2;
 };
 
-QW2DAnalyzer::QW2DAnalyzer(const edm::ParameterSet& pset) :
+QWNG2DAnalyzer::QWNG2DAnalyzer(const edm::ParameterSet& pset) :
 	srcPhi_(pset.getUntrackedParameter<edm::InputTag>("srcPhi")),
+	srcPt_(pset.getUntrackedParameter<edm::InputTag>("srcPt")),
 	srcEta_(pset.getUntrackedParameter<edm::InputTag>("srcEta")),
 	srcW_(pset.getUntrackedParameter<edm::InputTag>("srcW", std::string("NA")))
 {
 	consumes<std::vector<double> >(srcPhi_);
+	consumes<std::vector<double> >(srcPt_);
 	consumes<std::vector<double> >(srcEta_);
 	bWeight = false;
 
@@ -53,6 +62,11 @@ QW2DAnalyzer::QW2DAnalyzer(const edm::ParameterSet& pset) :
 	double hstart = pset.getUntrackedParameter<double>("hstart");
 	double hend = pset.getUntrackedParameter<double>("hend");
 
+	trigPtMin_ = pset.getUntrackedParameter<double>("trigPtMin");
+	trigPtMax_ = pset.getUntrackedParameter<double>("trigPtMax");
+	assoPtMin_ = pset.getUntrackedParameter<double>("assoPtMin");
+	assoPtMax_ = pset.getUntrackedParameter<double>("assoPtMax");
+
 	edm::Service<TFileService> fs;
 	h = fs->make<TH1D>("h", "h", hNbins, hstart, hend);
 	h->Sumw2();
@@ -63,14 +77,16 @@ QW2DAnalyzer::QW2DAnalyzer(const edm::ParameterSet& pset) :
 }
 
 void
-QW2DAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+QWNG2DAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 	using namespace edm;
 	Handle<std::vector<double> > phi;
+	Handle<std::vector<double> > pt;
 	Handle<std::vector<double> > eta;
 	Handle<std::vector<double> > w;
 
 	iEvent.getByLabel(srcPhi_, phi);
+	iEvent.getByLabel(srcPt_, pt);
 	iEvent.getByLabel(srcEta_, eta);
 	if ( bWeight ) {
 		iEvent.getByLabel(srcW_, w);
@@ -79,8 +95,10 @@ QW2DAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 	int sz = phi->size();
 	h->Fill(sz);
 	for ( int i = 0; i < sz; i++ ) {
+		if ( (*pt)[i] > trigPtMax_ or (*pt)[i] < trigPtMin_ ) continue;
 		for ( int j = 0; j < sz; j++ ) {
 			if ( i == j ) continue;
+			if ( (*pt)[j] > assoPtMax_ or (*pt)[j] < assoPtMin_ ) continue;
 			double Dphi = (*phi)[i] - (*phi)[j];
 			double Deta = (*eta)[i] - (*eta)[j];
 			while (Dphi > DphiMax) Dphi -= TMath::Pi()*2.;
@@ -95,6 +113,7 @@ QW2DAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 		// do mix
 
 		for ( int j = 0; j < int(mphi_.size()); j++ ) {
+			if ( (mpt_)[j] > assoPtMax_ or (mpt_)[j] < assoPtMin_ ) continue;
 			double Dphi = (*phi)[i] - (mphi_)[j];
 			double Deta = (*eta)[i] - (meta_)[j];
 			while (Dphi > DphiMax) Dphi -= TMath::Pi()*2.;
@@ -108,9 +127,10 @@ QW2DAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 		}
 	}
 	mphi_ = *phi;
+	mpt_ = *pt;
 	meta_ = *eta;
 	mw_ = *w;
 	return;
 }
 
-DEFINE_FWK_MODULE(QW2DAnalyzer);
+DEFINE_FWK_MODULE(QWNG2DAnalyzer);
