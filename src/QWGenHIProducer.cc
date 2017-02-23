943c8707eb1f3cb687fb73d7b28928cae6658b10
Quan Wang <BetterWang@gmail.com>
2017-02-22 22:30:02 -0600

    [+] update

---
+++ src/QWGenHIProducer.cc
@@ -0,0 +1,72 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "SimDataFormats/HiGenData/interface/GenHIEvent.h"
+
+#include "TFile.h"
+#include "TH2.h"
+#include "TMath.h"
+
+using namespace std;
+class QWGenHIProducer : public edm::EDProducer {
+public:
+	explicit QWGenHIProducer(const edm::ParameterSet&);
+	~QWGenHIProducer();
+
+private:
+	virtual void produce(edm::Event&, const edm::EventSetup&) override;
+	///
+
+	edm::InputTag	src_;
+
+};
+
+QWGenHIProducer::QWGenHIProducer(const edm::ParameterSet& pset) :
+	src_(pset.getUntrackedParameter<edm::InputTag>("src"))
+{
+	consumes<edm::GenHIEvent>(src_);
+
+	produces<double>("b");
+	produces<double>("EP");
+	produces<double>("Npart");
+	produces<double>("Ncoll");
+	produces<double>("ecc");
+}
+
+QWGenHIProducer::~QWGenHIProducer()
+{
+	return;
+}
+
+void QWGenHIProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	using namespace edm;
+
+	Handle<GenHIEvent> genHI;
+	iEvent.getByLabel( src_ , genHI ) ;
+
+	HepMC::GenEvent * genevt = (HepMC::GenEvent *)HepMCEvt->GetEvent();
+	GenHIEvent * hi = genHI->product();
+
+        auto_ptr<double> pb(new double(hi->b()));
+        auto_ptr<double> pEP(new double(hi->evtPlane()));
+        auto_ptr<double> pNpart(new double(hi->Npart()));
+        auto_ptr<double> pNcoll(new double(hi->Ncoll()));
+        auto_ptr<double> pecc(new double(hi->eccentricity()));
+
+	iEvent.put(pb, std::string("b"));
+	iEvent.put(pEP, std::string("EP"));
+	iEvent.put(pNpart, std::string("Npart"));
+	iEvent.put(pNcoll, std::string("Ncoll"));
+	iEvent.put(pecc, std::string("ecc"));
+
+}
+
+
+DEFINE_FWK_MODULE(QWGenHIProducer);
