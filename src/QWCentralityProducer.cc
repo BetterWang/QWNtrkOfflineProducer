fad4579e7000a3fb2340ca6d8ea0124f2992da6c
Quan Wang <BetterWang@gmail.com>
2017-07-10 06:01:34 +0200

    [+] centrality

---
+++ src/QWCentralityProducer.cc
@@ -0,0 +1,64 @@
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "DataFormats/HeavyIonEvent/interface/Centrality.h"
+
+#include "TFile.h"
+#include "TH2.h"
+#include "TMath.h"
+
+using namespace std;
+class QWCentralityProducer : public edm::EDProducer {
+public:
+	explicit QWCentralityProducer(const edm::ParameterSet&);
+	~QWCentralityProducer();
+
+private:
+	virtual void produce(edm::Event&, const edm::EventSetup&) override;
+	///
+	edm::InputTag	src_;
+};
+
+QWCentralityProducer::QWCentralityProducer(const edm::ParameterSet& pset) :
+	src_(pset.getUntrackedParameter<edm::InputTag>("src"))
+{
+	consumes<reco::Centrality>(src_);
+
+	produces< double >("EtHFtowerSum");
+	produces< double >("EtHFtowerSumPlus");
+	produces< double >("EtHFtowerSumMinus");
+}
+
+QWCentralityProducer::~QWCentralityProducer()
+{
+	return;
+}
+
+void QWCentralityProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	using namespace edm;
+	using namespace reco;
+	edm::Handle<reco::Centrality> cent;
+	iEvent.getByLabel(src_, cent);
+
+	double EtHFtowerSum = cent->EtHFtowerSum();
+	double EtHFtowerSumPlus = cent->EtHFtowerSumPlus();
+	double EtHFtowerSumMinus = cent->EtHFtowerSumMinus();
+
+	std::auto_ptr<double> pEtHFtowerSum(new double(EtHFtowerSum));
+	std::auto_ptr<double> pEtHFtowerSumPlus(new double(EtHFtowerSumPlus));
+	std::auto_ptr<double> pEtHFtowerSumMinus(new double(EtHFtowerSumMinus));
+
+	iEvent.put(pEtHFtowerSum, std::string("EtHFtowerSum"));
+	iEvent.put(pEtHFtowerSumPlus, std::string("EtHFtowerSumPlus"));
+	iEvent.put(pEtHFtowerSumMinus, std::string("EtHFtowerSumMinus"));
+}
+
+
+DEFINE_FWK_MODULE(QWCentralityProducer);
