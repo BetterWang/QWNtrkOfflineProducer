ff25d284ceeab876f6294fce1eab9bebba82f9e8
Quan Wang <BetterWang@gmail.com>
2018-03-22 16:23:34 -0500

    [+] QWDTagTreeMaker

---
+++ src/QWDTagTreeMaker.cc
@@ -0,0 +1,66 @@
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
+#include "TTree.h"
+#include "TMath.h"
+#include <iostream>
+#include <tuple>
+#include "string"
+#include "vector"
+
+
+class QWDTagTreeMaker : public edm::EDAnalyzer {
+public:
+	explicit QWDTagTreeMaker(const edm::ParameterSet&);
+	~QWDTagTreeMaker() {};
+private:
+	virtual void beginJob() {};
+	virtual void analyze(const edm::Event&, const edm::EventSetup&);
+	virtual void endJob() {};
+
+	edm::InputTag   src_;
+	std::vector<std::string>	vTags_;
+
+	TTree*	trV;
+	std::map< std::string, double* >	psv_;
+};
+
+QWDTagTreeMaker::QWDTagTreeMaker(const edm::ParameterSet& pset) :
+	src_(pset.getUntrackedParameter<edm::InputTag>("src")),
+	vTags_(pset.getUntrackedParameter< std::vector<edm::InputTag> >("vTag"))
+{
+	for ( auto & tag : vTags_ ) {
+		consumes<double>( tag );
+	}
+
+	edm::Service<TFileService> fs;
+	trV = fs->make<TTree>("trV", "trV");
+	for ( auto &tag : vTags_ ) {
+		std::string br = tag.instance() + "_" + tag.label();
+		psv_[br] = new double;
+		trV->Branch(br.c_str(), psv_[br]);
+	}
+}
+
+void
+QWDTagTreeMaker::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+	using namespace edm;
+	for ( auto & [key, val] : psv_ ) {
+		*val = 0.;
+	}
+	for ( auto &tag : vTags_ ) {
+		Handle<double> handle;
+		iEvent.getByLabel(tag, handle);
+		std::string br = tag.instance() + "_" + tag.label();
+		*(psv_[br]) = *handle;
+	}
+	trV->Fill();
+	return;
+}
+
+DEFINE_FWK_MODULE(QWDTagTreeMaker);
