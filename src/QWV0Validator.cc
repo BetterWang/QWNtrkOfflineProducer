94b53d6ff5dc24141d99a53ef40320491b8f56b7
Quan Wang <BetterWang@gmail.com>
2019-04-26 21:55:19 +0200

    [+] update

--- src/QWV0Validator.cc
+++ src/QWV0Validator.cc
@@ -105,20 +105,20 @@ class QWV0Validator : public edm::EDAnalyzer {
                 const reco::VertexCompositeCandidateCollection &collection,
                 const reco::SimToRecoCollection &simtorecoCollection);
 
-        // pt, eta, R?
-
+        // eff -- pt, eta, R
         std::array<TH3D *, 2> candidateEff_num_;
-        std::array<TH3D *, 2> candidateTkEff_num_;
-        std::array<TH3D *, 2> candidateFake_num_;
-        std::array<TH3D *, 2> candidateTkFake_num_;
-
-        std::array<TH3D *, 2> candidateFake_denom_;
         std::array<TH3D *, 2> candidateEff_denom_;
+
+        // fak -- pt, eta, mass
+        std::array<TH3D *, 2> candidateFake_num_;
+        std::array<TH3D *, 2> candidateTkFake_num_; // stauts=5, fake track caused fake V0
+        std::array<TH3D *, 2> candidateFake_denom_;
 
         std::array<TH1D *, 2> nCandidates_;
         std::array<TH1D *, 2> candidateStatus_;
         std::array<TH1D *, 2> fakeCandidateMass_;
         std::array<TH1D *, 2> candidateFakeDauRadDist_;
+        std::array<TH1D *, 2> candidateRealDauRadDist_;
         std::array<TH1D *, 2> candidateMassAll;
         std::array<TH1D *, 2> goodCandidateMass;
 
@@ -199,29 +199,26 @@ QWV0Validator::QWV0Validator(const edm::ParameterSet& iConfig) :
         Rbins.push_back(maxR);
     }
 
-    // pT, eta, R
+    // Ks Plots follow
+    // eff -- pt, eta, R
     auto fKs = fs->mkdir("Ks");
     candidateEff_num_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sEff_num", "K0sEff_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
+            "K0sEff_num", "K0sEff_num;p_{T};#eta;R", pTbins.size()-1, pTbins.data(),
             etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
-    candidateTkEff_num_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sTkEff_num", "K0sTkEff_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
-
     candidateEff_denom_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sEff_denom", "K0sEff_denom:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
+            "K0sEff_denom", "K0sEff_denom;p_{T};#eta;R", pTbins.size()-1, pTbins.data(),
             etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
 
+    // fak -- pt, eta, mass
     candidateFake_num_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sFake_num", "K0sFake_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
+            "K0sFake_num", "K0sFake_num;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vKsMass.size()-1, vKsMass.data() );
     candidateTkFake_num_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sTkFake_num", "K0sTkFake_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
-
+            "K0sTkFake_num", "K0sTkFake_num;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vKsMass.size()-1, vKsMass.data() );
     candidateFake_denom_[QWV0Validator::KSHORT] = fKs.make<TH3D>(
-            "K0sFake_denom", "K0sFake_denom:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
+            "K0sFake_denom", "K0sFake_denom;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vKsMass.size()-1, vKsMass.data() );
 
     nCandidates_[QWV0Validator::KSHORT] = fKs.make<TH1D>(
             "nK0s", "Number of K^{0}_{S} found per event", 60, 0., 60.);
@@ -229,57 +226,51 @@ QWV0Validator::QWV0Validator(const edm::ParameterSet& iConfig) :
             "ksMassFake", "Mass of fake K0S", ksMassNbins, minKsMass, maxKsMass);
     goodCandidateMass[QWV0Validator::KSHORT] = fKs.make<TH1D>(
             "ksMassGood", "Mass of good reco K0S", ksMassNbins, minKsMass, maxKsMass);
-    candidateMassAll[QWV0Validator::KSHORT] =
-        fKs.make<TH1D>("ksMassAll", "Invariant mass of all K0S", ksMassNbins,
-                minKsMass, maxKsMass);
+    candidateMassAll[QWV0Validator::KSHORT] = fKs.make<TH1D>(
+            "ksMassAll", "Invariant mass of all K0S", ksMassNbins, minKsMass, maxKsMass);
     candidateFakeDauRadDist_[QWV0Validator::KSHORT] = fKs.make<TH1D>(
-            "radDistFakeKs", "Production radius of daughter particle of Ks fake", 100,
-            0., 15.);
-    candidateStatus_[QWV0Validator::KSHORT] =
-        fKs.make<TH1D>("ksCandStatus", "Fake type by cand status", 10, 0., 10.);
+            "radDistFakeKs", "Production radius of daughter particle of Ks fake", 100, 0., 15.);
+    candidateRealDauRadDist_[QWV0Validator::KSHORT] = fKs.make<TH1D>(
+            "radDistRealKs", "Production radius of daughter particle of Ks real", 100, 0., 15.);
+    candidateStatus_[QWV0Validator::KSHORT] = fKs.make<TH1D>(
+            "ksCandStatus", "Fake type by cand status", 10, 0., 10.);
 
     // Lambda Plots follow
-
+    // eff -- pt, eta, R
     auto fLm = fs->mkdir("Lm");
     candidateEff_num_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamEff_num", "LmEff_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
-    candidateTkEff_num_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamTkEff_num", "LmTkEff_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
+            "LamEff_num", "LmEff_num;p_{T};#eta;R", pTbins.size()-1, pTbins.data(),
             etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
     candidateEff_denom_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamEff_denom", "LmEff_denom:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
+            "LamEff_denom", "LmEff_denom;p_{T};#eta;R", pTbins.size()-1, pTbins.data(),
             etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
 
+    // fak -- pt, eta, mass
     candidateFake_num_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamFake_num", "LmFake_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
+            "LamFake_num", "LmFake_num;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vLmMass.size()-1, vLmMass.data() );
     candidateTkFake_num_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamTkFake_num", "LmTkFake_num:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
+            "LamTkFake_num", "LmTkFake_num;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vLmMass.size()-1, vLmMass.data() );
     candidateFake_denom_[QWV0Validator::LAMBDA] = fLm.make<TH3D>(
-            "LamFake_denom", "LmFake_denom:p_{T}:#eta:R", pTbins.size()-1, pTbins.data(),
-            etaBins.size()-1, etaBins.data(), Rbins.size()-1, Rbins.data() );
+            "LamFake_denom", "LmFake_denom;p_{T};#eta;Mass", pTbins.size()-1, pTbins.data(),
+            etaBins.size()-1, etaBins.data(), vLmMass.size()-1, vLmMass.data() );
 
 
     nCandidates_[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
             "nLam", "Number of #Lambda^{0} found per event", 60, 0., 60.);
-    fakeCandidateMass_[QWV0Validator::LAMBDA] =
-        fLm.make<TH1D>("lamMassFake", "Mass of fake Lambda", lamMassNbins,
-                minLamMass, maxLamMass);
-    goodCandidateMass[QWV0Validator::LAMBDA] =
-        fLm.make<TH1D>("lamMassGood", "Mass of good Lambda", lamMassNbins,
-                minLamMass, maxLamMass);
-
-    candidateMassAll[QWV0Validator::LAMBDA] =
-        fLm.make<TH1D>("lamMassAll", "Invariant mass of all #Lambda^{0}",
-                lamMassNbins, minLamMass, maxLamMass);
+    fakeCandidateMass_[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
+            "lamMassFake", "Mass of fake Lambda", lamMassNbins, minLamMass, maxLamMass);
+    goodCandidateMass[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
+            "lamMassGood", "Mass of good Lambda", lamMassNbins, minLamMass, maxLamMass);
+    candidateMassAll[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
+            "lamMassAll", "Invariant mass of all #Lambda^{0}", lamMassNbins, minLamMass, maxLamMass);
     candidateFakeDauRadDist_[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
-            "radDistFakeLam", "Production radius of daughter particle of Lam fake",
-            100, 0., 15.);
-
-    candidateStatus_[QWV0Validator::LAMBDA] =
-        fLm.make<TH1D>("lamCandStatus", "Fake type by cand status", 10, 0., 10.);
+            "radDistFakeLam", "Production radius of daughter particle of Lam fake", 100, 0., 15.);
+    candidateRealDauRadDist_[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
+            "radDistRealLam", "Production radius of daughter particle of Lam real", 100, 0., 15.);
+    candidateStatus_[QWV0Validator::LAMBDA] = fLm.make<TH1D>(
+            "lamCandStatus", "Fake type by cand status", 10, 0., 10.);
 }
 
 QWV0Validator::~QWV0Validator() {}
@@ -292,11 +283,11 @@ void QWV0Validator::doFakeRates(
     using namespace edm;
 
     int numCandidateFound = 0;
-    int realCandidateFound = 0;
+    //int realCandidateFound = 0;
     double mass = 0.;
     float CandidatepT = 0.;
     float CandidateEta = 0.;
-    float CandidateR = 0.;
+    //float CandidateR = 0.;
     int CandidateStatus = 0;
     const unsigned int NUM_DAUGHTERS = 2;
     if (!collection.empty()) {
@@ -308,31 +299,30 @@ void QWV0Validator::doFakeRates(
             CandidatepT = (sqrt(iCandidate->momentum().perp2()));
             CandidateEta = iCandidate->momentum().eta();
             //CandidateEta = iCandidate->rapidity();
-            CandidateR = (sqrt(iCandidate->vertex().perp2()));
+            //CandidateR = (sqrt(iCandidate->vertex().perp2()));
             candidateMassAll[v0_type]->Fill(mass);
             CandidateStatus = 0;
 
             std::array<reco::TrackRef, NUM_DAUGHTERS> theDaughterTracks = {
                 {(*(dynamic_cast<const reco::RecoChargedCandidate*>(
                                 iCandidate->daughter(0)))).track(),
-                (*(dynamic_cast<const reco::RecoChargedCandidate*>(
-                                                                   iCandidate->daughter(1)))).track()}};
+                 (*(dynamic_cast<const reco::RecoChargedCandidate*>(
+                                iCandidate->daughter(1)))).track()}
+            };
 
             TrackingParticleRef tpref;
-            TrackingParticleRef firstDauTP;
+            //TrackingParticleRef firstDauTP;
             TrackingVertexRef candidateVtx;
 
-            std::array<double, NUM_DAUGHTERS> radDist;
+            std::array<double, NUM_DAUGHTERS> radDist{{-1., -1.}};
             // Loop through candidate's daugher tracks
-            for (View<reco::Track>::size_type i = 0; i < theDaughterTracks.size();
-                    ++i) {
-                radDist = {{-1., -1.}};
+            for (View<reco::Track>::size_type i = 0; i < theDaughterTracks.size(); ++i)
+            {
                 // Found track from theDaughterTracks
                 RefToBase<reco::Track> track(theDaughterTracks.at(i));
 
                 if (recotosimCollection.find(track) != recotosimCollection.end()) {
-                    const std::vector<std::pair<TrackingParticleRef, double> >& tp =
-                        recotosimCollection[track];
+                    const std::vector<std::pair<TrackingParticleRef, double> >& tp = recotosimCollection[track];
                     if (!tp.empty()) {
                         tpref = tp.begin()->first;
 
@@ -349,12 +339,10 @@ void QWV0Validator::doFakeRates(
                                             CandidateStatus = 6;
                                         }
 
-                                        for (TrackingVertex::tp_iterator iTP =
-                                                parentVertex->sourceTracks_begin();
-                                                iTP != parentVertex->sourceTracks_end(); iTP++) {
+                                        for (TrackingVertex::tp_iterator iTP = parentVertex->sourceTracks_begin(); iTP != parentVertex->sourceTracks_end(); iTP++) {
                                             if (abs((*iTP)->pdgId()) == particle_pdgid) {
                                                 CandidateStatus = 1;
-                                                realCandidateFound++;
+                                                //realCandidateFound++;
                                                 numCandidateFound += 1.;
                                                 goodCandidateMass[v0_type]->Fill(mass);
                                             } else {
@@ -380,28 +368,39 @@ void QWV0Validator::doFakeRates(
                                 // to compare to the parentVertex from the second
                                 // track
                                 candidateVtx = parentVertex;
-                                firstDauTP = tpref;
+                                //firstDauTP = tpref;
                             }
                         }  // parent vertex is null
-                    }    // check on associated tp size zero
+                    } else {   // check on associated tp size zero
+                        CandidateStatus = 8;
+                    }
                 } else {
+                    // fake track
                     CandidateStatus = 5;
                 }
             }  // Loop on candidate's daughter tracks
 
             // fill the fake rate histograms
             if (CandidateStatus > 1) {
-                candidateFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+                //candidateFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+                candidateFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, mass);
                 candidateStatus_[v0_type]->Fill((float)CandidateStatus);
                 fakeCandidateMass_[v0_type]->Fill(mass);
                 for (auto distance : radDist) {
                     if (distance > 0) candidateFakeDauRadDist_[v0_type]->Fill(distance);
                 }
             }
-            if (CandidateStatus == 5) {
-                candidateTkFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+            if (CandidateStatus == 1) {
+                for (auto distance : radDist) {
+                    if (distance > 0) candidateRealDauRadDist_[v0_type]->Fill(distance);
+                }
             }
-            candidateFake_denom_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+            if (CandidateStatus == 5) {
+                //candidateTkFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+                candidateTkFake_num_[v0_type]->Fill(CandidatepT, CandidateEta, mass);
+            }
+            //candidateFake_denom_[v0_type]->Fill(CandidatepT, CandidateEta, CandidateR);
+            candidateFake_denom_[v0_type]->Fill(CandidatepT, CandidateEta, mass);
         }  // Loop on candidates
     }    // check on presence of candidate's collection in the event
     nCandidates_[v0_type]->Fill((float)numCandidateFound);
